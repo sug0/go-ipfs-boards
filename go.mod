760a80a616574cb7ec6f1f3628a47717062b4b12
sugoiuguu <sugoiuguu@tfwno.gf>
2020-05-21 19:10:15 +0100

    gossip on the new threads

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/sug0/go-ipfs-boards
 
 go 1.14
 
-require github.com/ipfs/go-ipfs-api v0.0.3
+require (
+	github.com/ipfs/go-ipfs-api v0.0.3
+	github.com/julienschmidt/httprouter v1.3.0
+	nhooyr.io/websocket v1.8.6
+)
--- main/web/main.go
+++ main/web/main.go
@@ -3,52 +3,130 @@ package main
 import (
     "io"
     "os"
+    "log"
     "net/http"
     "os/signal"
 
-    //"nhooyr.io/websocket"
-    //"nhooyr.io/websocket/wsjson"
+    "nhooyr.io/websocket"
+    "nhooyr.io/websocket/wsjson"
 
-    //"github.com/sug0/go-ipfs-boards/gossip"
-    //"github.com/sug0/go-ipfs-boards/boards"
+    "github.com/julienschmidt/httprouter"
+
+    "github.com/sug0/go-ipfs-boards/boards"
+    "github.com/sug0/go-ipfs-boards/gossip"
 )
 
-const indexPage = `<html>
+const boardsIndexPage = `<html>
     <head>
-        <title>damn</title>
+        <title>aew mermaum</title>
         <meta charset="utf8"/>
     </head>
     <body>
-        ganda jarda mano é isso ai
+        <div id="threads"/>
         <script type="application/javascript">
-            console.log('sweet');
+            let threads = document.getElementById('threads');
+            let board = window.location.pathname;
+            let ws = new WebSocket('ws://localhost:8989/ws' + board);
+            ws.onmessage = e => {
+                let post = JSON.parse(e.data);
+                if (!post) return;
+                let postDiv = document.createElement('div');
+                postDiv.id = 'post';
+                let pHeader = document.createElement('h1');
+                pHeader.id = 'post-header';
+                pHeader.innerText = post.Title + ' | ' + post.Posted;
+                let pContent = document.createElement('p');
+                pContent.id = 'post-content';
+                pContent.innerText = post.Content;
+                postDiv.appendChild(pHeader);
+                postDiv.appendChild(pContent);
+                threads.appendChild(postDiv);
+            };
         </script>
         <style>
-            body {
-                color: #ffffff;
-                background: #020202;
-            }
         </style>
     </body>
 </html>`
 
+var (
+    postGossip *gossip.Gossip
+    client     *boards.Client
+)
+
 func main() {
+    var err error
+
+    postGossip, err = gossip.NewGossip()
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer postGossip.Close()
+
+    client, err = boards.NewClient()
+    if err != nil {
+        log.Fatal(err)
+    }
+
     sig := make(chan os.Signal, 1)
     signal.Notify(sig, os.Interrupt)
 
-    http.HandleFunc("/", indexHandler)
-    http.HandleFunc("/ws", wsHandler)
+    router := httprouter.New()
+    router.GET("/boards/*board", boardsHandler)
+    router.GET("/threads/:thread", threadsHandler)
+    router.GET("/ws/boards/*board", wsHandlerBoards)
+    router.GET("/ws/threads/:thread", wsHandlerThreads)
 
     go func() {
-        panic(http.ListenAndServe(":8989", nil))
+        log.Fatal(http.ListenAndServe(":8989", router))
     }()
     <-sig
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-    io.WriteString(w, indexPage)
+func boardsHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    io.WriteString(w, boardsIndexPage)
 }
 
-func wsHandler(w http.ResponseWriter, r *http.Request) {
-    io.WriteString(w, "for now I do nothing")
+func wsHandlerBoards(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    if ps.ByName("board") == "/" {
+        http.Error(w, "not a board", http.StatusBadRequest)
+        return
+    }
+    c, err := websocket.Accept(w, r, &websocket.AcceptOptions{InsecureSkipVerify: true})
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    defer c.Close(websocket.StatusNormalClosure, "bye")
+    board := ps.ByName("board")[1:]
+    err = postGossip.AddBoardWhitelist(board)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    defer postGossip.DelBoardWhitelist(board)
+
+    // the parent context
+    ctx := r.Context()
+    closeCtx := c.CloseRead(ctx)
+
+    for {
+        select {
+        case <-closeCtx.Done():
+            return
+        case adv := <-postGossip.Threads():
+            p, err := client.GetPost(adv.Ref)
+            if err != nil {
+                continue
+            }
+            wsjson.Write(ctx, c, p)
+        }
+    }
+}
+
+func threadsHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    // nothing
+}
+
+func wsHandlerThreads(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    // nothing
 }
