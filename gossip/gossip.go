f2deffb84f77df2128006022c5374891756f7302
sugoiuguu <sugoiuguu@tfwno.gf>
2020-05-22 17:57:36 +0100

    fix bug

--- gossip/gossip.go
+++ gossip/gossip.go
@@ -9,14 +9,19 @@ import (
     "github.com/sug0/go-ipfs-boards/boards"
 )
 
+type subscription struct {
+    count int
+    sub   *ipfs.PubSubSubscription
+}
+
 type Gossip struct {
     shell *ipfs.Shell
 
     boards  chan Advertisement
     threads chan Advertisement
 
-    boardsMap  map[string]*ipfs.PubSubSubscription
-    threadsMap map[string]*ipfs.PubSubSubscription
+    boardsMap  map[string]*subscription
+    threadsMap map[string]*subscription
 
     boardsMux  sync.Mutex
     threadsMux sync.Mutex
@@ -40,7 +45,8 @@ func advertise(sub *ipfs.PubSubSubscription, advertChan chan<- Advertisement) {
 func (g *Gossip) AddBoardWhitelist(board string) error {
     g.boardsMux.Lock()
     defer g.boardsMux.Unlock()
-    if _, ok := g.boardsMap[board]; ok {
+    if s := g.boardsMap[board]; s != nil {
+        s.count++
         return nil
     }
     var topic string
@@ -54,7 +60,7 @@ func (g *Gossip) AddBoardWhitelist(board string) error {
         err = fmt.Errorf("gossip: failed to sub to board: %w", err)
         return err
     }
-    g.boardsMap[board] = sub
+    g.boardsMap[board] = &subscription{sub: sub, count: 1}
     go advertise(sub, g.boards)
     return nil
 }
@@ -62,7 +68,8 @@ func (g *Gossip) AddBoardWhitelist(board string) error {
 func (g *Gossip) AddThreadWhitelist(thread string) error {
     g.threadsMux.Lock()
     defer g.threadsMux.Unlock()
-    if _, ok := g.threadsMap[thread]; ok {
+    if s := g.threadsMap[thread]; s != nil {
+        s.count++
         return nil
     }
     var topic string
@@ -76,29 +83,31 @@ func (g *Gossip) AddThreadWhitelist(thread string) error {
         err = fmt.Errorf("gossip: failed to sub to thread: %w", err)
         return err
     }
-    g.threadsMap[thread] = sub
+    g.threadsMap[thread] = &subscription{sub: sub, count: 1}
     go advertise(sub, g.threads)
     return nil
 }
 
 func (g *Gossip) DelBoardWhitelist(board string) {
     g.boardsMux.Lock()
-    sub := g.boardsMap[board]
-    delete(g.boardsMap, board)
-    g.boardsMux.Unlock()
-    if sub != nil {
-        sub.Cancel()
+    s := g.boardsMap[board]
+    s.count--
+    if s.count == 0 {
+        s.sub.Cancel()
+        delete(g.boardsMap, board)
     }
+    g.boardsMux.Unlock()
 }
 
 func (g *Gossip) DelThreadWhitelist(thread string) {
     g.threadsMux.Lock()
-    sub := g.threadsMap[thread]
-    delete(g.threadsMap, thread)
-    g.threadsMux.Unlock()
-    if sub != nil {
-        sub.Cancel()
+    s := g.threadsMap[thread]
+    s.count--
+    if s.count == 0 {
+        s.sub.Cancel()
+        delete(g.threadsMap, thread)
     }
+    g.threadsMux.Unlock()
 }
 
 func (g *Gossip) Threads() <-chan Advertisement {
@@ -111,16 +120,16 @@ func (g *Gossip) Posts() <-chan Advertisement {
 
 func (g *Gossip) Close() error {
     g.boardsMux.Lock()
-    for k, sub := range g.boardsMap {
+    for k, s := range g.boardsMap {
         delete(g.boardsMap, k)
-        sub.Cancel()
+        s.sub.Cancel()
     }
     g.boardsMux.Unlock()
 
     g.threadsMux.Lock()
-    for k, sub := range g.threadsMap {
+    for k, s := range g.threadsMap {
         delete(g.threadsMap, k)
-        sub.Cancel()
+        s.sub.Cancel()
     }
     g.threadsMux.Unlock()
 
@@ -137,8 +146,8 @@ func NewGossip() (*Gossip, error) {
         shell: sh,
         boards: make(chan Advertisement, 8),
         threads: make(chan Advertisement, 8),
-        boardsMap: make(map[string]*ipfs.PubSubSubscription),
-        threadsMap: make(map[string]*ipfs.PubSubSubscription),
+        boardsMap: make(map[string]*subscription),
+        threadsMap: make(map[string]*subscription),
     }
     return g, nil
 }
